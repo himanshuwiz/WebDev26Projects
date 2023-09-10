40da3849579dc5d3d5c76e540b986b0de11b5864
Himanshu Dhingra <himdhi2003@gmail.com>
2023-09-11 02:23:56 +0530

    Added Data

---
+++ Project4/Project 4/main.js
@@ -0,0 +1,27 @@
+const passwordBox = document.getElementById("password");
+const length = 10;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbols = "!@#$%^&*()_+{}[]<>|\+_<<..~`` ";
+
+const allChars = upperCase + lowerCase +number+symbols;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
